6abbd0d223e95c8fdb7640e5f3ec74881b2613f6
Yas Okada <yas.okadatech@gmail.com>
2024-06-14 10:25:22 +0900

    support ORGANIZATION_SELECT and GROUP_SELECT

--- src/main/java/org/embulk/output/kintone/KintoneColumnType.java
+++ src/main/java/org/embulk/output/kintone/KintoneColumnType.java
@@ -286,7 +286,8 @@ public enum KintoneColumnType {
 
     @Override
     public OrganizationSelectFieldValue getFieldValue(String value, KintoneColumnOption option) {
-      throw new UnsupportedOperationException();
+      return DESERIALIZER.deserialize(
+          value.isEmpty() ? "[]" : value, OrganizationSelectFieldValue.class);
     }
 
     @Override
@@ -296,7 +297,7 @@ public enum KintoneColumnType {
 
     @Override
     protected List<Type> getSupportedTypes() {
-      return Collections.emptyList();
+      return Collections.singletonList(Types.JSON);
     }
   },
   GROUP_SELECT {
@@ -307,7 +308,7 @@ public enum KintoneColumnType {
 
     @Override
     public GroupSelectFieldValue getFieldValue(String value, KintoneColumnOption option) {
-      throw new UnsupportedOperationException();
+      return DESERIALIZER.deserialize(value.isEmpty() ? "[]" : value, GroupSelectFieldValue.class);
     }
 
     @Override
@@ -317,7 +318,7 @@ public enum KintoneColumnType {
 
     @Override
     protected List<Type> getSupportedTypes() {
-      return Collections.emptyList();
+      return Collections.singletonList(Types.JSON);
     }
   },
   DATE {
